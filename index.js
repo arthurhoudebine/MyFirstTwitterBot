1d383519f5d4309960d2bc2d8882f7a1c5b4613f
Arthur Houdebine <arthur.houdebine@gmail.com>
2018-12-15 15:50:53 +0100

    xx

---
+++ index.js
@@ -0,0 +1,31 @@
+var Twit = require('twit')
+var T = new Twit({
+    consumer_key:         'TpUq0D4KLldyfyl2hgThVItzk',
+    consumer_secret:      'DScuTtO9fHQq6EZMdBCAdqLA6M2jeIE1L2EYdc1gFGkJJmpffN',
+    access_token:         '2926848095-IiwBfJlZ9YZuy6XhuhzPnXOrnIXpf9IvDX0zRpJ',
+    access_token_secret:  'KkV478Br6BvZhcSf0pvblLdlimh7RLfc3nvmcLfvvfKww',
+})
+
+// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//     console.log(data)
+//   })  
+  
+// T.post('statuses/retweet/:id', { id: '1073754910900944897' }, function (err, data, response) {
+//     console.log(data)
+//   })  
+
+var users = ["3608373454","986331243410350080"];
+
+var stream = T.stream('statuses/filter', {follow: users});
+
+
+stream.on('tweet', function (tweet) {
+    if (users.indexOf(tweet.user.id_str) > -1) {
+        console.log(tweet.user.name + ": " + tweet.text);
+        T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
+            console.log(data)
+        })
+    }
+})
+
+
